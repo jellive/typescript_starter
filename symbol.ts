a9abab3ff17b2f45d75415d7caf3481e68981f1e
PD Jell <mystral7@naver.com>
2021-04-30 16:27:13 +0900

    add symbol

---
+++ symbol.ts
@@ -0,0 +1,1 @@
+console.log(Symbol('foo') === Symbol('foo')) // Symbol은 객체에서 특정한 키 값을 만들 때 (접근을 제한하고 싶을 때) 주로 사용한다.
---
+++ test.js
@@ -0,0 +1,4 @@
+"use strict";
+var a = 'Mark';
+function hello(b) { }
+hello(29);
