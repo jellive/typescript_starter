2628db938ba89f16cfe7d0f835207b61e592766c
PD Jell <>
2021-05-28 16:57:45 +0900

    add generic basic

---
+++ src/generic/generic2.ts
@@ -0,0 +1,6 @@
+function helloBasic<T, U>(message: T, comment: U): T {
+  return message
+}
+
+helloBasic<string, number>('Mark', 39)
+helloBasic(36, 39) // 가장 좁은 범위의 타입을 추론하기 때문에, number 타입이 아니라 36이라는 타입을 고정한다.
