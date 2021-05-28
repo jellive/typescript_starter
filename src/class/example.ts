a26b3be7e483d79fce0f08dea49613f97d4b2176
PD Jell <>
2021-05-28 11:35:45 +0900

    add initializer

--- src/class/example.ts
+++ src/class/example.ts
@@ -1,10 +1,20 @@
 class ClassPerson {
-  name
-  constructor(name: string) {
-    this.name = name
+  name: string = 'Mark'
+  age: number
+  //   age: number = 0
+  // constructor(name: string) {
+  //   this.name = name
+  // }
+  constructor(age?: number) {
+    // constructor는 async-await를 사용할 수 없다.
+    if (!age) {
+      this.age = 20
+    } else {
+      this.age = age
+    }
   }
 }
 
-const cp1 = new ClassPerson('Mark')
+const cp1 = new ClassPerson()
 
 console.log(cp1)
