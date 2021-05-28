8779672e38fcbc286e55d6f156449ddb44b1806d
PD Jell <>
2021-05-28 15:04:50 +0900

    add static properties and methods

---
+++ src/class/example3.ts
@@ -0,0 +1,12 @@
+class HPerson {
+  public static hello() {
+    console.log('안녕하세요')
+  }
+  static CITY = 'Seoul'
+}
+
+const hp1 = new HPerson() // 안 쓰임.
+// hp1.hello()
+
+HPerson.hello()
+console.log(HPerson.CITY)
