77b2b5c95c2306f1ef82c0a998353aa03a7babcf
PD Jell <>
2021-05-28 13:52:25 +0900

    add getters and setters

--- .git.sh
+++ .git.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 read -p "Enter git message: " message
+echo "git adding"
 git add .
+echo "git comitting. message:" $message
 git commit -m "$message"
+echo "pushing"
 git push -u origin master
